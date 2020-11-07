c1aa052fa35527bacb0686a11792786d9bc55366
Pragith Prakash <pragith@gmail.com>
2020-11-07 02:48:23 -0400

    added pycairo examples for drawing on the card template using Python

--- cards_generator.py
+++ cards_generator.py
@@ -153,16 +153,29 @@ def draw_3(cr, width, height):
     cr.set_source_rgba(0.5, 0, 0, 0.5)
     cr.stroke()
 
+def draw_rect(cr, width, height):
+    cr.scale(width, height)
+    cr.set_line_width(0.04)
+
+    cr.rectangle(0.1, 0.1, 0.6, 0.6)
+    cr.set_line_width(0.03)
+    cr.set_source_rgb(0.8, 0.8, 0.8)
+    cr.fill()
+
+    cr.push_group()
+    cr.rectangle(0.3, 0.3, 0.6, 0.6)
+    cr.set_source(cairo.SolidPattern(1, 0, 0))
+    cr.fill_preserve()
+    cr.set_source(cairo.SolidPattern(0, 0, 0))
+    cr.stroke()
+    cr.pop_group_to_source()
+    cr.paint_with_alpha(0.5)
+
 #draw_1(context, WIDTH, HEIGHT)
-draw_2(context, WIDTH, HEIGHT)
+#draw_2(context, WIDTH, HEIGHT)
 #draw_3(context, WIDTH, HEIGHT)
+draw_rect(context, WIDTH, HEIGHT)
 
 surface.write_to_png("decks/png/test1.png")  # Output to PNG
 # %%
-# %%
-
-
-
-# %%
-
 # %%
